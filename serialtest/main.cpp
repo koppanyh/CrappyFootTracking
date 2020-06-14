4e0eeebfb5f92f14f57002c98ff8c8283f52d9d6
Koppany Horvath <koppanyh12@yahoo.com>
2020-06-13 18:18:41 -0700

    Fixed PC serial test code
    The test code now prints out the values from the Arduino without either missing bytes or having padded 0 bytes.

--- serialtest/main.cpp
+++ serialtest/main.cpp
@@ -1,33 +1,31 @@
-#define DATA_LENGTH 256
-
 #include <iostream>
 #include "SerialPort.h"
-#include <windows.h>
-
+//#include <windows.h>
 using namespace std;
+
+#define DATA_LENGTH 256
 
 int main(void)
 {
-    char com[32] = "hello world";
-    cout << "COM number: ";
-    cin >> com;
-    cout << "Number: " << com << endl;
-    for(int i=0; i<32; i++){
-        cout << (int) com[i] << " " << com[i] << endl;
-    }
-    return 0;
+    char com[32] = "\\\\.\\COM3";
+    /*cout << "COM number: "; //uncomment this if you want to be able to type in the com number
+    cin >> com + 7;*/
 
     char receivedString[DATA_LENGTH];
-    SerialPort* arduino = new SerialPort("\\\\.\\COM3");
-    int hasRead = 1;
+    SerialPort* arduino = new SerialPort(com);
 
-    while(arduino->isConnected() && hasRead){
-        Sleep(5);
-        hasRead = arduino->readSerialPort(receivedString, DATA_LENGTH);
-        //while(arduino->isConnected() && !arduino->readableSize());
-        if (hasRead) //cout << (int) receivedString[255]; // << endl;
-            for(int i=0; i<DATA_LENGTH; i++)
-                cout << (int) receivedString[i] << ' ';
-        else cerr << endl << endl << "Error occured reading data" << endl;
+    int c = 0;
+
+    while(arduino->isConnected()){
+        if(c >= 40){ //after 40 prints use a new line
+            cout << endl;
+            c = 0;
+        }
+        //Sleep(5);
+        int hasRead = arduino->readSerialPort(receivedString, DATA_LENGTH);
+        if(!hasRead) //don't print anything if there's nothing to print
+            continue;
+        cout << (int) receivedString[hasRead-1] << ' '; //print the newest read and discard other data
+        c++;
     }
 }
